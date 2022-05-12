5f46b3d4ebef43798f0fecc0e31bb152206a3745
Alexander Iatsenko <iatsalexo@gmail.com>
2022-05-12 22:59:45 +0300

    added HW29

---
+++ Lesson_29/assets/js/script.js
@@ -0,0 +1,5 @@
+document.querySelector("#firstName").innerHTML += prompt("Введіть ваше ім'я");
+document.querySelector("#lastName").innerHTML += prompt("Введіть ваше прізвище");
+document.querySelector("#age").innerHTML += prompt("Введіть ваш вік");
+document.querySelector("#city").innerHTML += prompt("Введіть ваш місто");
+document.querySelector("#job").innerHTML += prompt("Введіть ким працюєте");
