3c850a81ba320fbc5cb49144719e732bc99a778b
Alexander Iatsenko <iatsalexo@gmail.com>
2022-06-03 00:24:39 +0300

    changed HW34

--- Lesson_34/assets/js/script.js
+++ Lesson_34/assets/js/script.js
@@ -1,23 +1,34 @@
 function setOrder(){
-	let inputs = document.querySelectorAll("input");
-	let result = document.querySelector('#result');
-	count++;
 	let div = document.createElement('div');
 	div.className = 'order mb';
-	let pCountOrder = document.createElement('p')
-	pCountOrder.innerText = `Замовлення №${count}`;
+	let pCountOrder = document.createElement('p');
+	pCountOrder.innerHTML = `<strong>Замовлення №${count}</strong>`;
 	div.append(pCountOrder);
 	result.append(div);
 	for(let el of inputs){
-		if(el.id !== 'btn'){
-			let p = document.createElement("p");
-			p.innerText = `${el.placeholder}: ${el.value}`;
-			div.append(p);
-			el.value = '';
+		if(el.value === '') {
+			alert( 'Введіть всі данні!' );
+			div.remove();
+			inputs.forEach(e => {
+				if(e.id !== 'btn') e.value = '';
+			});
+			return;
+		}
+		else{
+			if(el.id !== 'btn'){
+				let p = document.createElement("p");
+				p.innerText = `${el.placeholder}: ${el.value}`;
+				div.append(p);
+				el.value = '';
+			}
 		}
 	}
+	count++;
 }
 
-let count = 0;
+
+let inputs = document.querySelectorAll("input");
+let result = document.querySelector('#result');
+let count = 1;
 document.querySelector('#btn')
 		.addEventListener('click', setOrder);
